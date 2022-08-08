d498caede3dab8265a2f5009b5167ac963a509aa
Nikita Bolshakov <itbolshakov@yandex.ru>
2022-08-08 23:06:08 +0300

    feat: DnD into BurgerContainer && captions

--- src/services/actions/burger-constructor.jsx
+++ src/services/actions/burger-constructor.jsx
@@ -1,2 +1,4 @@
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_BUN = 'ADD_BUN';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+export const MOVE_ELEMENT = 'MOVE_ELEMENT';
--- src/services/reducers/burger-constructor.jsx
+++ src/services/reducers/burger-constructor.jsx
@@ -1,8 +1,8 @@
-import { ADD_BUN, ADD_INGREDIENT } from "../actions/burger-constructor";
+import { ADD_BUN, ADD_INGREDIENT, DELETE_INGREDIENT, MOVE_ELEMENT } from "../actions/burger-constructor";
 
 const initialState = {
   currentIngredients: [],
-  buns: [],
+  currentBun: [],
 };
 
 export const burgerConstructorReducer = (state = initialState, action) => {
@@ -13,7 +13,24 @@ export const burgerConstructorReducer = (state = initialState, action) => {
         currentIngredients: [...state.currentIngredients, action.payload],
       };
     case ADD_BUN:
-      return { ...state, buns: action.payload };
+      return { ...state, currentBun: action.payload };
+    case DELETE_INGREDIENT:
+      return {
+        ...state,
+        currentIngredients: [...state.currentIngredients].filter(ingredient => ingredient.id !== action.id),
+      };
+      case MOVE_ELEMENT: {
+        const dragContainer = [...state.currentIngredients];
+        dragContainer.splice(
+          action.payload.dragIndex, 
+          0,                        
+          dragContainer.splice(action.payload.hoverIndex, 1)[0]
+        );
+        return {
+          ...state,
+          currentIngredients: dragContainer
+        };
+      }
     default:
       return state;
   }
